5b113e86044498b73b5e5b31374decb7a2bc3cc0
June TDS <tds.mm.dev004@gmail.com>
2024-04-11 14:59:27 +0700

    fix bug

--- app/api/route.ts
+++ app/api/route.ts
@@ -4,14 +4,14 @@ import download from '@/store/download.json'
  
 export async function GET() {
   
-  const file = await fs.readFile('public/data/download.json', 'utf8')
+  const file = await fs.readFile('/data/download.json', 'utf8')
   const count = parseInt(JSON.parse(file).count)
   return NextResponse.json({ count })
 }
 
 export async function POST() {
-  const file = await fs.readFile('public/data/download.json', 'utf8')
+  const file = await fs.readFile('/data/download.json', 'utf8')
   const count = parseInt(JSON.parse(file).count) + 1
-  await fs.writeFile('public/data/download.json', JSON.stringify({ count: count }))
+  await fs.writeFile('/data/download.json', JSON.stringify({ count: count }))
   return NextResponse.json({ count })
 }
