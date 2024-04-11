75ec81c08de024ca913a83dfd6eb499c7180c7b9
June TDS <tds.mm.dev004@gmail.com>
2024-04-11 14:51:32 +0700

    Move json file

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,15 +1,17 @@
 import { NextResponse } from 'next/server'
 import { promises as fs } from 'fs'
+import download from '@/store/download.json'
  
 export async function GET() {
-  const file = await fs.readFile(process.cwd() + '/store/download.json', 'utf8')
+  
+  const file = await fs.readFile('public/data/download.json', 'utf8')
   const count = parseInt(JSON.parse(file).count)
   return NextResponse.json({ count })
 }
 
 export async function POST() {
-  const file = await fs.readFile(process.cwd() + '/store/download.json', 'utf8')
+  const file = await fs.readFile('public/data/download.json', 'utf8')
   const count = parseInt(JSON.parse(file).count) + 1
-  await fs.writeFile(process.cwd() + '/store/download.json', JSON.stringify({ count: count }))
+  await fs.writeFile('public/data/download.json', JSON.stringify({ count: count }))
   return NextResponse.json({ count })
 }
