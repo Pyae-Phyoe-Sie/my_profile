ed7c0c22028e2693aaa999e8c7e970783472ef4b
June TDS <tds.mm.dev004@gmail.com>
2024-04-12 23:43:58 +0700

    Add count

--- services/CountService.ts
+++ services/CountService.ts
@@ -1,20 +1,3 @@
-// "use server"
-// import { promises as fs } from 'fs'
-
-// export async function downloadCount() {
-//     const file = await fs.readFile(process.cwd() + '/store/download.json', 'utf8')
-//     const count = parseInt(JSON.parse(file).count) + 1
-//     await fs.writeFile(process.cwd() + '/store/download.json', JSON.stringify({ count: count }))
-//     return count
-// }
-
-// export async function getCount() {
-//     const file = await fs.readFile(process.cwd() + '/store/download.json', 'utf8')
-//     const count = parseInt(JSON.parse(file).count)
-//     console.log(count)
-//     return count
-// }
-
 import firebase from '@/app/firebase'
 import { getDatabase, ref, set, onValue, child, get } from "firebase/database"
 
@@ -30,13 +13,3 @@ export async function getCount() {
 export async function updateCount(value: number) {
     set(child(dbRef, `/download`), value);
 }
-
-// const starCountRef = ref(db, '/count/Download');
-// console.log(starCountRef)
-// onValue(starCountRef, (snapshot) => {
-//     console.log(snapshot)
-//     const data = snapshot.val();
-//     console.log("data", data)
-// });
-
-// export default db
